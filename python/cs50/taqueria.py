cfcf489da311ceabd90bf1573d53a955bbd055fd
Thales Silva <thales.silva@obuc.com.br>
2023-10-07 09:32:40 -0300

    Test commit.

--- python/cs50/taqueria.py
+++ python/cs50/taqueria.py
@@ -8,7 +8,8 @@ menu = {
     "Super Burrito": 8.50,
     "Super Quesadilla": 9.50,
     "Taco": 3.00,
-    "Tortilla Salad": 8.00
+    "Tortilla Salad": 8.00,
+    "Tortilla": 1.00,
 }
 
 def main():    
