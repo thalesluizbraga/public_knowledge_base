06e4dac163a4147a1385dc80eec0ede8348409f0
Thales Luiz <thales.silva@obuc.com.br>
2023-10-15 21:42:01 -0300

    professor.py changed to corrected version

--- python/cs50/professor.py
+++ python/cs50/professor.py
@@ -1,3 +1,37 @@
+# One of David’s first toys as a child, funny enough, was Little Professor, a “calculator” that would generate ten different
+# math problems for David to solve. For instance, if the toy were to display 4 + 0 = , David would (hopefully) answer with
+# 4. If the toy were to display 4 + 1 = , David would (hopefully) answer with 5. If David were to answer incorrectly, the
+# toy would display EEE. And after three incorrect answers for the same problem, the toy would simply display the correct
+# answer (e.g., 4 + 0 = 4 or 4 + 1 = 5).
+
+# In a file called professor.py, implement a program that:
+
+# Prompts the user for a level, n If the user does not input 1, 2, or 3, the program should prompt again. - ok
+# Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with
+# digits. No need to support operations other than addition (+).
+# Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), the program should
+# output EEE and prompt the user again, allowing the user up to three tries in total for that problem. If the user has
+# still not answered correctly after three tries, the program should output the correct answer.
+# The program should ultimately output the user’s score: the number of correct answers out of 10.
+# Structure your program as follows, wherein get_level prompts (and, if need be, re-prompts) the user for a level and
+# returns 1, 2, or 3, and generate_integer returns a randomly generated non-negative integer with level digits or raises a
+# ValueError if level is not 1, 2, or 3:
+#
+# import random
+#
+#
+# def main():
+    # ...
+# def get_level():
+    # ...
+
+# def generate_integer(level):
+    # ...
+
+# if __name__ == "__main__":
+    # main()
+
+
 import random
 
 
@@ -28,30 +62,40 @@ def generate_integer(level):
     return x, y
 
 def main():
-    count = 10
     level = get_level()
-    wrong_answers = 0
-    right_answers = 0
+    score = 0
 
-    while count > 0:
-
+    for i in range(10):
         x,y = generate_integer(level)
-        print(f"{x} + {y}")
-        answer = int(input('Answer: '))
+        attempts = 0 # counter pro while
 
-        if answer != x + y:
-            wrong_answers += 1
-            print('EEE')
-            if wrong_answers == 3:
-                print( x + y)
-        else:
-            right_answers += 1
+        while attempts < 3:
+            print(f'{x} + {y} = ')
 
-        count -= 1
+            try:
+                answer = int(input())
 
-    print('Your score is:', right_answers)
+                if answer == x + y:
+                    score += 1
+                    break
 
-# Tem que fazer um counter dentro do while pra ir diminuindo ate a decima jogada e depois somar o score (sendo cada jogada valendo um ponto)
+                else:
+                    attempts += 1
+                    if attempts == 3:
+                        print(f'{x} + {y} = ', x + y)
+                    else:
+                        print('EEE')
+
+            except ValueError:
+                attempts += 1
+                if attempts == 3:
+                    print(f'{x} + {y} = ', x + y)
+                else:
+                    print('EEE')
+
+
+    print('Score: ', score)
 
 if __name__ == '__main__':
     main()
+
