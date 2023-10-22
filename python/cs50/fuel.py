07eebdbed2c0cb417211f87cd7f480be034204aa
Thales Luiz <thales.silva@obuc.com.br>
2023-10-22 11:43:17 -0300

    test_fuel.py added

--- python/cs50/fuel.py
+++ python/cs50/fuel.py
@@ -1,48 +1,31 @@
-# Fuel gauges indicate, often with fractions, just how much fuel is in a tank. For instance 1/4 indicates that a 
-# tank is 25% full, 1/2 indicates that a tank is 50% full, and 3/4 indicates that a tank is 75% full.
+# In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y,  wherein each of X and Y is an integer, - OK
+# and then outputs, as a percentage rounded to the nearest integer,
+# how much fuel is in the tank. If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty. And if 99% or more remains, output F instead to indicate that the tank is essentially full. - OK
+# If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.
 
-# In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y, wherein each
-# of X and Y is an integer, and then outputs, as a percentage rounded to the nearest integer, how much fuel is in 
-# the tank. If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty. And if
-# 99% or more remains, output F instead to indicate that the tank is essentially full.
 
-# If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not
-# necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.
-
-def calculate_fuel_percentage(fraction):
-    try:
-        x, y = map(int, fraction.split('/'))
-
-        if (y == 0) or (x > y): # Condiçoes do or entre parenteses pra nao dar ruim.
-            raise ValueError # Condiçao forte 
-
-        fuel_percentage = (x / y) * 100
-
-        if fuel_percentage <= 1:
-            return 'E'
-        elif fuel_percentage >= 99:
-            return 'F'
+def convert(fraction):
+    if "/" in fraction:
+        x, y = fraction.split("/")
+        if int(x) > int(y) and int(y) != 0: raise ValueError()
         else:
-            return str(round(fuel_percentage)) + '%'
+            div = int(x) / int(y)
+            return round(div * 100)
 
-    except (ValueError, ZeroDivisionError):
-        print('Invalid input. Please enter a valid fraction (X/Y).')
-        return None
 
+def gauge(percentage):
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
 
 def main():
-    while True:
-        fraction = input('Enter a fraction (X/Y): ')
-        fuel_status = calculate_fuel_percentage(fraction)
+    print(gauge(convert(input("Fraction: "))))
 
-        if fuel_status is not None:
-            print(fuel_status)
-            break
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
-# Se tiver mais de uma funçao pra chamar na main() ela vai confundir, entao por isso tem que usar essa expressao
-# Aqui o try/except ficar fora do loop, porque eu so quero validar uma regra, e nao continuar recebendo um resultado dentro do loop (um counter por exemplo)
+
 
