0916fbabb2a5d49056befc4fba120cc809fd40c5
Thales <thalesluizbraga@gmail.com>
2024-02-16 17:47:40 -0300

    changes in fuel.py and coke.py files

--- python/cs50/coke.py
+++ python/cs50/coke.py
@@ -6,26 +6,25 @@
 # change the user is owed. Assume that the user will only input integers, and ignore any integer that isn’t an
 # accepted denomination.
 
-count = 50
-print('Amount Due:',count)
+def counter(amount_rule, amount_due):
 
-
-while count > 0:
-    coin = int(input('Insert Coin:'))
-    amount_due = count - coin
-    invalid_coin = 0
-
-    if amount_due <= 0:
-        change_owed = abs(amount_due)
-        print('Change Owed:',change_owed)
-        break
-
-    else:
-        if coin in [25,10,5]:
-            count -= coin
-            print('Amount Due:', count)
-        else:
-            print('Amount Due:', count)
-
-
+    while amount_due < 50:
+            amount_owed = amount_rule - amount_due
+            coin = int(input('Insert a coin: '))
+        
+            if coin not in [25,10,5]:
+                print('Moeda nao aceita, insira outra')
+            else:
+                amount_due += coin
+                if amount_due > amount_rule:
+                    print(f'Amount Due: {amount_rule}')
+                    print(f'Amount Owed: {amount_owed}')
+                else:
+                    print(f'Amount Due:{amount_due}')
+        
+    
+if __name__ == '__main__':
+    amount_due = 0
+    amount_rule = 50
+    counter(amount_rule, amount_due)
 
