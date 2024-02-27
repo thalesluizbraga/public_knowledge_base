1dec863e286a4483732c5f343759a521c9b1982a
Thales <thalesluizbraga@gmail.com>
2024-02-27 14:08:28 -0300

    lines.py modified and poo.py added

--- python/cs50/lines.py
+++ python/cs50/lines.py
@@ -1,33 +1,57 @@
-# importar sys
-# importar csv???
+# importar sys - ok
 # se len(argv) < 2:
     # return 'Too few arguments'
 # se len(argv) > 2:
     # return 'Too many arguments'
 # se arquivo nao encontrado no diretorio:
     # raise FileNotFoundError
-# clausula with para iniciar o contexto de leitura do arquivo (tipo 'r')
-# Se row is blank:
+
+
+# clausula with para iniciar o contexto de leitura do arquivo (tipo 'r') - ok
+# Se row is blank: - ok
+    # nao contar - ok
+# Se row begins with # - ok
     # nao contar
-# Se row begins with #
-    # nao contar
-# contar linhas
+# contar linhas - ok
+
+#import sys
+#
+
+#print(count)
 
 import sys
 
-with open('C:/Users/Thales/Documents/repos/public_knowledge_base/python/cs50/lines.txt', 'r') as file:
-    count = 0
+def get_arg():
+
+    if len(sys.argv) > 2:
+        return 'Too many arguments.'
+    elif len(sys.argv) < 2:
+        return 'Too few arguments.'
+    elif not sys.argv[1].endswith('.py'):
+        return 'Not a python file.'
+    else:   
+        return sys.argv[1]
     
-    for row in file:
-        if row.startswith('#'):
-            continue
-        elif row.strip():
-            continue
-        else: 
-            count += 1
+            
+if __name__ == '__main__':
+    arg = get_arg()
+    
+    try:
 
-print(count)
+        with open(arg, 'r') as file:
+            count = 0    
+            for row in file:
+                if row.startswith('#'):
+                    continue
+                elif row.isspace():
+                    continue
+                else: 
+                    count += 1
+                    
+        print(f'Total of lines in file: {count}')
 
+    except FileNotFoundError:
+        raise 'File not Found.'
 
 
 
