9b74e65de075e33459ed1538439e50a3570f9e6b
Thales Luiz <thalesluizbraga@gmail.com>
2024-02-27 23:05:14 -0300

    bonus.py and professor.py formated in PEP8 standars

--- python/bootcamp_jornada_dados/bonus.py
+++ python/bootcamp_jornada_dados/bonus.py
@@ -12,6 +12,7 @@ def get_name():
         except ValueError:
             return 'Invalid name. Please enter a valid name'
 
+
 def get_salary():
     while True:
         try:
@@ -23,15 +24,17 @@ def get_salary():
         except ValueError:
             return 'Invalid salary. Please enter a valid number.'
 
+
 def calculate_bonus(salary: float, bonus: dict):
     if 0 < salary <= 2000:
         return salary * (1 + bonus['faixa_1'])
     elif 2000 < salary <= 5000:
-        return  salary * (1 + bonus['faixa_2'])
+        return salary * (1 + bonus['faixa_2'])
     elif 5000 < salary <= 10000:
         return salary * (1 + bonus['faixa_3'])
     else:
-        return salary * (1 + bonus['faixa_4'])        
+        return salary * (1 + bonus['faixa_4'])
+
 
 if __name__ == '__main__':
     bonus = {'faixa_1': 0.5, 'faixa_2': 1, 'faixa_3': 1.5, 'faixa_4': 2}
