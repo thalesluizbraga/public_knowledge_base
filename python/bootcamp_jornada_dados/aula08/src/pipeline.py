a5d47cce31e2657d02e6650efb63f33bcdbbd779
Thales <thalesluizbraga@gmail.com>
2024-03-01 11:29:08 -0300

    load_df function adjusted in pipeline.py

--- python/bootcamp_jornada_dados/aula08/src/pipeline.py
+++ python/bootcamp_jornada_dados/aula08/src/pipeline.py
@@ -1,12 +1,11 @@
-# %%
+#%%
 import os
 import pandas as pd
 
-
-def extract_json(path: str) -> list:
+def extract_json(path: str) -> list: 
     df_list = []
     for file in os.listdir(path):
-        if file.endswith(".json"):
+        if file.endswith('.json'):
             df = pd.read_json(os.path.join(path, file))
             df_list.append(df)
     return df_list
@@ -16,27 +15,25 @@ def concat_json(df_list: list) -> pd.DataFrame:
     df = pd.concat(df_list)
     return df
 
-
 def calculate_total_sales_col(df: pd.DataFrame) -> pd.DataFrame:
-    df["Total"] = df["Quantidade"] * df["Venda"]
+    df['Total'] = df['Quantidade'] * df['Venda']
     return df
 
-
-def load_df(df: pd.DataFrame, format_output: list):
+def load_df(df: pd.DataFrame, path_output: str ,format_output: list):
     for formato in format_output:
-        if formato == "csv":
-            df = df.to_csv("data.csv", index=False)
-            print("Csv file saved successfully.")
-        if formato == "parquet":
-            df = df.to_parquet("data.parquet", index=False)
-            print("Parquet file saved successfully.")
+        if formato == 'csv':
+            df = df.to_csv(f'{path_output}data.csv', index=False)
+            print('Csv file saved successfully.')
+        if formato == 'parquet':
+            df = df.to_parquet(f'{path_output}data.parquet', index=False)
+            print('Parquet file saved successfully.')
 
 
-if __name__ == "__main__":
-    path = "../public_knwoledge_base/python/bootcamp_jornada_dados/aula08/data"
+if __name__ == '__main__':
+    path = 'C:/Users/JHS/Documents/repos/public_knwoledge_base/python/bootcamp_jornada_dados/aula08/data'
+    path_output = 'C:/Users/JHS/Documents/repos/public_knwoledge_base/python/bootcamp_jornada_dados/aula08/data'
     df_list = extract_json(path)
     df = concat_json(df_list)
     df = calculate_total_sales_col(df)
-    load_df(df, "csv")
-
+    print(load_df(df, path_output, 'csv' ))
 # %%
