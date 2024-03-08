e5249a52527ebe3650aa93bcf6e52cdfd7511406
Thales Luiz <thalesluizbraga@gmail.com>
2024-03-07 23:38:16 -0300

    advancing with pizza.py file

--- python/cs50/pizza.py
+++ python/cs50/pizza.py
@@ -1,3 +1,9 @@
+#%%
+
+# with open read 
+# passar para dicionario
+# passar tabulate par formatar o dicionario como tabulacao
+
 
 import sys
 
@@ -12,23 +18,35 @@ def get_arg():
     else:   
         return sys.argv[1]
     
-                
+def read_csv():
+
+
+
 if __name__ == '__main__':
     arg = get_arg()
     
-    try:
+    
+# %%
+import csv
+from tabulate import tabulate
 
-        with open(arg, 'r') as file:
-            count = 0    
-            for row in file:
-                if row.startswith('#'):
-                    continue
-                elif row.isspace():
-                    continue
-                else: 
-                    count += 1
-                    
-        print(f'Total of lines in file: {count}')
+with open('sicilian.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
 
-    except FileNotFoundError:
-        raise 'File not Found.'
+
+# %%
+# conseguir fazer em pandas, pra depois entender o codigo fonte
+# da funcao read_csv e replicar no exercicio
+
+import pandas as pd
+from tabulate import tabulate
+df = pd.read_csv('regular.csv')
+df_dict = df.to_dict()
+
+#print(tabulate(df_dict, headers='keys', tablefmt='grid'))
+#print(tabulate(df, headers='keys', tablefmt='grid'))
+
+
+# %%
