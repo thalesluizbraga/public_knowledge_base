053a2cfe4cb7c7ef00418f25bf20033dfa77ddb9
Thales Luiz <thalesluizbraga@gmail.com>
2024-03-19 23:56:42 -0300

    exercicios aula 2 finalizados

--- python/bootcamp_jornada_dados/aula01a06/exercicios_aula2.py
+++ python/bootcamp_jornada_dados/aula01a06/exercicios_aula2.py
@@ -1,6 +1,6 @@
 #%%
 
-# Exercício 23: Calculadora Simples
+# Calculadora Simples
 
 #Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário.
 # Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a
@@ -30,7 +30,7 @@ print(calculator(2,'+',0))
     
 # %%
 
-#Exercício 24: Classificador de Números
+# Classificador de Números
 #Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para 
 #classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
 
@@ -52,5 +52,29 @@ def classificador(num):
         'Input nao é um numero'
 
 print(classificador('a'))
+
+# %%
+
+# Conversão de Tipo com Validação
+
+# Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de 
+# entrada em uma lista de números inteiros. Utilize try-except para tratar a conversão de cada número e validar que cada 
+# elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de 
+# erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
+
+
+def validacao(list_num):
+
+    try:
+        for num in range(len(list_num)): # para iterar em listas vai ser sempre for i in range(len()):
+            list_num[num] = int(list_num[num])
+            return list_num 
+    except:
+        return 'conversao falhou'
+
+
+print(validacao(['1','2','3']))
+
+
 
 # %%
