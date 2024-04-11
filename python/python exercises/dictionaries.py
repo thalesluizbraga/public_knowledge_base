fcdd4030756876082e82a72f202a659f95991fc2
Thales Luiz <thalesluizbraga@gmail.com>
2024-04-10 21:48:45 -0300

    exercicios de dicionario adicionados

--- python/python exercises/dictionaries.py
+++ python/python exercises/dictionaries.py
@@ -18,13 +18,11 @@ print(dict1['city'])
 # Merge these dictionaries into a single dictionary 
 #representing the combined inventory of both stores.
 
-dict_a: dict = {'item': 'bike', 'qtd': 1}
-dict_b: dict = {'item': 'shoes' ,'qtd': 3}
-# dict_merged: dict = dict_a | dict_b # Se fizer assim, o dict_b sobrepoe o dict_a... nao da certo 
-dict_merged: dict = {**dict_a, **dict_b}
-print(dict_a)
-print(dict_b)
+dict_a: dict = {'shoes': 10, 'bike': 1}
+dict_b: dict = {'hat': 4 , 'ball': 3} # A chave tem que ser unica sempre, se nao vai ser sobreposta pelo registro posterior sempre
+dict_merged: dict = dict_a | dict_b
 print(dict_merged)
+
 
 # %%
 
@@ -36,4 +34,18 @@ print(dict_merged)
 
 for k in dict_merged.items():
     print(k)    
+
 # %%
+# Dictionary Manipulation:
+# This exercise involves adding, updating, and removing items
+# from a dictionary, which requires understanding basic dictionary
+# manipulation methods.
+
+# Adding
+
+
+
+# Updating
+
+# Removing
+
